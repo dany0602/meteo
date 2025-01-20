1f043cf3cfd80595095b104a696feb9a6b8683d2
danielleclerc <daniel.leclerc0602@gmail.com>
2025-01-20 15:11:43 +0100

    appli terminée

--- script.js
+++ script.js
@@ -12,43 +12,39 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        console.log(data.city.name)
-        console.log()
-        console.log()
-        console.log(data.list[0].main.temp_max)
-        console.log(data.list[0].main.temp_min)
-        console.log()
-        console.log()
+        console.log(data.cod)
+        // console.log(data.city.name)
+        // console.log()
+        // console.log()
+        // console.log(data.list[0].main.temp_max)
+        // console.log(data.list[0].main.temp_min)
+        // console.log()
+        // console.log(city)
 
+if (data.cod == 404){
+  document.getElementById('404').classList.remove('notfound')
+} else {
+  document.getElementById('404').classList.add('notfound')
+  document.querySelector('#interface').innerHTML=`
+  <div class="soleil">
+  <p class="temperature">${data.list[0].main.temp}°c</p>
+  <div class="date">
+  <p class="city">${data.city.name}</p>
+    <p>${moment(data.list[0].dt_txt).locale("fr").format('L')}</p>
+    <p>min : ${data.list[0].main.temp_min}°c / max : ${data.list[0].main.temp_max}°c </p>
+  </div>
+</div>
 
+<div class="pluie">
+  <img class="satellite" src="./asset/th.jpg" alt="image satellite" />
+  <h1>Température ressentie : ${data.list[0].main.feels_like}° C</h1>
+    <p>${data.list[0].weather[0].description}</p>
+  <img class="iconMeteo" src="https://openweathermap.org/img/wn/10d@2x.png" alt="img">
 
+</div>
+`
 
-
-        document.querySelector('#interface').innerHTML=`
-            <div class="soleil">
-            <p class="temperature">${data.list[0].main.temp}°c</p>
-            <div class="date">
-            <p class="city">${data.city.name}</p>
-              <p>${moment(data.list[0].dt_txt).locale("fr").format('L')}</p>
-              <p>min:${data.list[0].main.temp_min}°c / max:${data.list[0].main.temp_max}°c </p>
-            </div>
-          </div>
-
-          <div class="pluie">
-            <img class="satellite" src="./asset/th.jpg" alt="image satellite" />
-            <h1>Température ressentie :${data.list[0].main.feels_like}° C</h1>
-            <img class="iconMeteo" src="https://openweathermap.org/img/wn/10d@2x.png" alt="img">
-            <p>${data.list[0].weather[0].description}</p>
-          </div>
-        `
-
-
-
-
-
-
-
-
+}
     })
 
 
